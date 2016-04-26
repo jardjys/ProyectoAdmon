0d948bf75b33279957d94e2f63e5a679f4a03648
César Robles <chechare_94@hotmail.com>
2016-04-26 14:51:34 -0500

    Checklist to PDF :D

--- app/scripts/checklist.js
+++ app/scripts/checklist.js
@@ -1,22 +1,32 @@
 function printChecklist() {
   var doc = new jsPDF('landscape');
+  var element = $('#checklist_form').get(0);
+  var cachedElement = $('#checklist_form').clone();
 
-  var specialElementHandlers = {
-    '.material_icons': function(element, renderer){
-		    return true;
-	    }
+  for(var i = 1; i <= 20; i++){
+    $('[name='+i+']').parent().html($('[name='+i+']').val());
+  }
+
+  var handlers = {
+    '#ignore': function(el, renderer){
+      console.log("ignore");
+      return true;
+    }
   };
 
   doc.setFontSize(36);
   doc.text("Project Checklist", 100, 25);
 
-  doc.fromHTML($('#checklist_form').get(0), 40, 50, {
+  doc.fromHTML(element, 40, 50, {
 	   'width': 170,
-     'elementHandlers': specialElementHandlers
+     'elementHandlers': handlers
   });
 
-  doc.save('Checklist.pdf');
+  $('#checklist_form').html(cachedElement)
+  doc.output("dataurlnewwindow");
+  //doc.save('Checklist.pdf');
 }
+
 
 function cleanChecklist() {
   for(var i = 1; i <= 20; i++){
