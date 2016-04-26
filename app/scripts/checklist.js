bcb9370d83d2199423805287a96ceeff1054df0b
César Robles <chechare_94@hotmail.com>
2016-04-26 03:46:07 -0500

    Previous PDF print format

--- app/scripts/checklist.js
+++ app/scripts/checklist.js
@@ -1,4 +1,21 @@
-function save() {
+function printChecklist() {
+  var doc = new jsPDF('landscape');
+
+  var specialElementHandlers = {
+    '.material_icons': function(element, renderer){
+		    return true;
+	    }
+  };
+
+  doc.setFontSize(36);
+  doc.text("Project Checklist", 100, 25);
+
+  doc.fromHTML($('#checklist_form').get(0), 40, 50, {
+	   'width': 170,
+     'elementHandlers': specialElementHandlers
+  });
+
+  doc.save('Checklist.pdf');
 }
 
 function cleanChecklist() {
